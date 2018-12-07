f82005abd92d1eb87ab3a3daeb5ccebd4971a891
Unknown <vale@valerauko.net>
2018-12-08 00:15:23 +0900

    Log fetch separately

--- fetch.clj
+++ fetch.clj
@@ -1,16 +1,20 @@
 (ns bark.fetch
   (:require [manifold.deferred :as md]
             [aleph.http :as http]
+            [bark.core :as core]
             [bark.json :refer [parse-json]]))
 
 (defn fetch-resource
   [uri]
-  (md/chain
-    (http/get uri {:headers {:accept "application/activity+json"}
-                   :connect-timeout 1000
-                   :read-timeout 2000})
-    :body
-    parse-json))
+  (core/retry
+    #(md/chain
+       (http/get uri {:headers {:accept "application/activity+json"}
+                      :connect-timeout 1000
+                      :read-timeout 2000})
+       :body
+       parse-json)
+    {:logger-fn (core/make-logger {:type "fetch"
+                                   :remote-addr uri})}))
 
 (defn deref-object
   [object find-object]
